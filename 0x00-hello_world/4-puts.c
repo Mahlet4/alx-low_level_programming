3c0a5c3e67ffa69582eedafa90cec8e3d8da8ee6
Mahlet291 <mahletkumelachew1@gmail.com>
2021-08-26 11:51:58 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /**
- * main - pritnt "Programming is like building a multilingual puzzle using puts func
+ * main - pritnt  using puts func
+ *
  * Return: Always 0.
  */
 int main(void)
