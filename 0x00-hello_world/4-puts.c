25eeec3b8dc89098d5cafd057b734e4ca60db8c0
Mahlet291 <mahletkumelachew1@gmail.com>
2021-08-26 11:28:19 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * pritnt "Programming is like building a multilingual puzzle using puts func
+ *
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
