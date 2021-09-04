f8afa3e74d28d7f741713458f4f73036f7877736
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-04 13:09:27 -0700

    adding main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef "MAIN_H"
+#define MAIN_H
 #include <unistd.h>
 /**
  * _putchr - writes the character c to stdout
@@ -10,3 +12,4 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
+#endif
