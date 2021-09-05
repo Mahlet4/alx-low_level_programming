c65f8bac5a0b96c23ac8e3e489cabc973f66f535
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-05 01:44:38 -0700

    10x lower case letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@ void print_alphabet_x10(void)
 {
 char letter;
 int i = 0;
-while (i<10)
+while (i < 10)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
+_putchar('\n');
 }
 i++;
 }
-_putchar('\n');
 }
