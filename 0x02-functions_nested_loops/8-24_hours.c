c1a2d12d02a40fdc3293da17af81ae7b72884d86
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-05 06:02:31 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * jack_bauer(void)
+ */
+void jack_bauer(void)
+{
+int n,x,y,z;
+for (n = 0; n <= 2; n++)
+{
+for (x=0; x <= 10; x++)
+{
+if (x == 10)
+{
+_putchar(n + '0');
+_putchar(0 + '0');
+}
+else
+{
+_putchar(n + '0');
+_putchar(x + '0');
+}
+_putchar(':')
+for (y = 0; y <= 5; y++)
+{
+for (z = 0; z <= 10; z++)
+{
+if (z == 10)
+{
+_putchar(y + '0');
+_putchar(0 + '0');
+}
+else
+{
+_putchar(y + '0');
+_putchar(z + '0');
+}
+}
+}
+}
+_putchar('\n');
+}
+}
