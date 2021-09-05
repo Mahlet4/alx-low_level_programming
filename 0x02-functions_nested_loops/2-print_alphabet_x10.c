6b0efaf4c84269b700e80d1879b8cced3cab26c5
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-05 01:40:05 -0700

    10x lower case letters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints 10 times the alphabet in lower case.
+ *
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int i = 0;
+while (i<10)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+i++;
+}
+_putchar('\n');
+}
