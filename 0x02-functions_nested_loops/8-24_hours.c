fcc1df1eb4a4187d75ad02e4fd109a7cd1d8b8f4
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-05 09:50:00 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer(void)
+ * jack_bauer(void)- print hrs and minutes
  */
 void jack_bauer(void)
 {
@@ -9,10 +9,10 @@ for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
 {
-_putchar((hour/10) + '0');
+_putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
-_putchar((minute/10) + '0');
+_putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
 }
