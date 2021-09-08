dc6bdf8cfae7c2cbd97bc404b43ca2d6e45156f7
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-08 06:13:27 -0700

    printing more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers- prints numbers
+ */
+void more_numbers(void)
+{
+int n;
+for (n = 0; n <= 14; n++)
+{
+for (n = 0; n <= 14; n++)
+{
+_putchar(n + '0');
+}
+}
+_putchar('\n');
+}
