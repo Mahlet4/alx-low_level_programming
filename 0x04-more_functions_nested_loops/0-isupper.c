7e322c7e4c76a465e11023ee49fbc145a14d5f10
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-08 04:21:34 -0700

    uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper- checkes for upper case character
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+for (c = 65; c <= 90; c++)
+{
+return (1);
+}
+return (0);
+}
