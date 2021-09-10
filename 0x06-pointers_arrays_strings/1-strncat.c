83a133e9616605feaa51d97ee4828f76b8035e2c
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-10 11:58:31 -0700

    concatenating strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _srncat- concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: the integer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int len = 0; i = 0;
+while (dest[len] != '\0')
+{
+len++;
+}
+while (i < n && src[i])
+{
+dest[len] = src[i];
+len++;
+i++
+}
+dest[len + n + 1] = '\0';
+return (dest);
+}
