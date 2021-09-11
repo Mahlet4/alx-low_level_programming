78fa75f4271c36c5ee350f2beecf0aecef54580f
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-11 09:20:59 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
  */
 void more_numbers(void)
 {
-  int n, x;
+int n, x;
 for (n = 0; n <= 14; n++)
 {
 for (x = 0; x <= 14; x++){
@@ -12,7 +12,7 @@ if (x > 9)
 {
 _putchar(1 + '0');
 }
-_putchar(n + '0');
+_putchar(x + '0');
 }
 _putchar('\n');
 }
