2b21e560808684aea0048093ebd9b87b2f4dd1c9
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-11 10:41:31 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line- prints lines
+ * @n: the number of times '_' is printed
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+int x = 0;
+while (x <= n)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
