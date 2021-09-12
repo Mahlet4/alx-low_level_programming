e1a70508808333113ea84b6997d39d9cf719291f
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-12 12:53:47 -0700

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal- prints diagonalline
+ * @n: the number of times '\' is printed
+ */
+void print_diagonal(int n)
+{
+char line[];
+int i;
+if (n = 0 && n < 0)
+{
+_putchar('\n');
+}
+else{
+for (i = 0; i < n; i++)
+{
+int x;
+while (line[i])
+{
+x++
+}
+line[x] = ' ';
+int y = x - 1;
+line[y] = '\';
+_putchar(line[x]);
+_putchar('\n');
+}
+}
+}
