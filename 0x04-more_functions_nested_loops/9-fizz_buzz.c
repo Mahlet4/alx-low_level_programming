6208924e49bb6bad19b31605853d4887e58e1bc6
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-13 02:33:56 -0700

    print numbers from 1-100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,27 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 != 0 && i % 5 != 0)
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
 {
 printf("%d", i);
+}
 if (i == 100)
 {
 continue;
 }
 printf(" ");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-printf(" ");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-printf(" ");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-printf(" ");
-}
 }
 printf("\n");
 return (0);
