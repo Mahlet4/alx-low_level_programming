b22508f38feb0d66fd9d277f106f45fac90cf239
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-13 00:29:26 -0700

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,18 +5,18 @@
  */
 void print_diagonal(int n)
 {
-  int len, space;
-  if (n > 0)
-    {
-      for (len = 0; len < n; len++)
-	{
-	  for (space = 0; space < len; space++)
-	    {
-	      _putchar(' ');
-	    }
-	  _putchar('\\');
-	  _putchar('\n');
-	}
-    }
-  _putchar('\n');
+int len, space;
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+{
+for (space = 0; space < len; space++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+_putchar('\n');
+}
 }
