c8c7c9a04d693082b5b91cef991df93e01051aaf
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-15 07:10:39 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@ void print_times_table(int n)
 {
 if (n < 15 && n >= 0)
 {
-int i, x;
+int i, x, y = 0;
 for (i = 0; i <= n; i++)
 {
 for (x = 0; x <= n; x++)
@@ -15,14 +15,22 @@ for (x = 0; x <= n; x++)
 if (i * x <= 9)
 {
 _putchar((i * x) + '0');
+while(y < 3)
+{
+_putchar(' ');
+y++;
+}
 }
 else if (i * x < 100)
 {
+_putchar(' ');
+_putchar(' ');
 _putchar((i * x / 10) % 10 + '0');
 _putchar((i * x) % 10 + '0');
 }
 else
 {
+_putchar(' ');
 _putchar((i * x / 100) % 10 + '0');
 _putchar((i * x / 10) % 10 + '0');
 _putchar((i * x) % 10 + '0');
@@ -31,24 +39,7 @@ if (x == n)
 {
 continue;
 }
-if (i * x <= 9)
-{
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-else if (i * x < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-}
 }
 _putchar('\n');
 }
