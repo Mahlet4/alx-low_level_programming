f199db7df376a5052eee86f4ac45e27c1d53a80f
Mahlet291 <mahletkumelachew1@gmail.com>
2021-09-16 13:35:20 -0700

    calculates the square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion- returns the square root of a number
- * @n: the number to calculate the square root of
- * Return: the final answer
  * _sqrt - calculates the square root of a number
  * @num: the number to find the square root of
  * @y: number to test
@@ -20,6 +17,11 @@ return (-1);
 }
 return (_sqrt(num, y + 1));
 }
+/**
+ * _sqrt_recursion- returns the square root of a number
+ * @n: the number to calculate the square root of
+ * Return: the final answer
+ */
 int _sqrt_recursion(int n)
 {
 int y = 0;
