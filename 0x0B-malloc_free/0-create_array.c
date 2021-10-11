dc4dad0c9d751d42b1d4ea2d111275ac35007b9c
Mahlet291 <mahletkumelachew1@gmail.com>
2021-10-11 13:06:44 -0700

    allocating memory with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,24 +4,25 @@
  * create_array - creates an array of chars
  * @size: size of the array to be initialized
  * @c: the character to initialize the array with
- * Return: NULL if size = 0 or the function fails, a pointer to the array if otherwise
+ * Return: NULL if size = 0 or the function fails, 
+           a poiner to the array if otherwise
  */
 char *create_array(unsigned int size, char c)
 {
 char *array;
 unsigned int i;
-if(size == 0)
+if (size == 0)
 {
-return(NULL);
+return (NULL);
 }
-array = malloc(sizeof(char)*size);
-if(array == 0)
+array = malloc(sizeof(char) * size);
+if (array == 0)
 {
-return(NULL);
+return (NULL);
 }
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 array[i] = c;
 }
-return(array);
+return (array);
 }
