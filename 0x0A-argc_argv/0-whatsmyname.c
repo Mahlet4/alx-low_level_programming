eaf5cc60996dd8721229248b7fd229e1a30707cf
Mahlet291 <mahletkumelachew1@gmail.com>
2021-11-08 06:05:50 -0800

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: An array of pointers to the arguments
  * Return: 0
  */
-int main(int __attribute__((__unused__)) argc, char* argv)
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
