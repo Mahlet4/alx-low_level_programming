0b76ca882881244f81db11a2262e1696663a2259
Mahlet291 <mahletkumelachew1@gmail.com>
2021-11-08 16:48:55 -0800

    adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0, x = 0;
+for (i = 1; i < argc; i++)
+{
+if (argv[i] < '0' || argv[i] > '9')
+{
+x++;
+}
+}
+if (x == (argc - 1))
+{
+printf("0\n");
+}
+else if (x > 0 && x < (argc - 1))
+{
+printf("Error\n");
+return (1);
+}
+else if (x == 0)
+{
+for (i = 1; i < argc; i++)
+{
+sum = sum + atoi(argv[i]);
+printf("%d\n", sum);
+}
+}
+return (0);
+}
