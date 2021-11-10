c14c88a4c8548517b14eb4cfdeb0e8066a9b15be
Mahlet291 <mahletkumelachew1@gmail.com>
2021-11-10 00:41:58 -0800

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - defines a new type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
